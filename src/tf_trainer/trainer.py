2b25e56a082baed51a1cfabbc952e4c0523b9e0c
Alexander Susha <isushik94@gmail.com>
2018-10-03 12:14:07 +0300

    removed name_scope from freeze

--- src/tf_trainer/trainer.py
+++ src/tf_trainer/trainer.py
@@ -391,7 +391,7 @@ class Trainer:
 
     def freeze(self, input_getter, outputs_names=None,
                training_dir_path=None, ckpt_path=None, graph_protected_nodes=None,
-               model_scope='model', verbose=False):
+               verbose=False):
         """
         """
         if not input_getter:
@@ -414,18 +414,16 @@ class Trainer:
         graph = tf.Graph()
         with graph.as_default():
             var_scope = tf.get_variable_scope() if self._var_scope is None else self._var_scope
-            scope = graph.get_name_scope() if not model_scope else model_scope
-            with tf.variable_scope(var_scope) as vs:
-                with tf.name_scope(scope):
-                    inputs = input_getter()
-                    if not isinstance(inputs, (tuple, list)):
-                        inputs = [inputs]
+            with tf.variable_scope(var_scope):
+                inputs = input_getter()
+                if not isinstance(inputs, (tuple, list)):
+                    inputs = [inputs]
 
-                    model = self._model_getter()
-                    if hasattr(model, 'inference') and callable(model.inference):
-                        outputs = model.inference(*inputs)
-                    else:
-                        outputs = model.forward(False, *inputs)
+                model = self._model_getter()
+                if hasattr(model, 'inference') and callable(model.inference):
+                    outputs = model.inference(*inputs)
+                else:
+                    outputs = model.forward(False, *inputs)
 
                 if not isinstance(outputs, (tuple, list)):
                     outputs = [outputs]
